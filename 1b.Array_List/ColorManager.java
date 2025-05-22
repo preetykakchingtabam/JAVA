64c506c10aa9e3540be5003150922b4ea68e237e
preetykakchingtabam <pitykakchingtabam@gmail.com>
2025-05-22 23:19:02 +0530

    Update ColorManager.java

--- 1b.Array_List/ColorManager.java
+++ 1b.Array_List/ColorManager.java
@@ -1,3 +1,6 @@
+// Write a java program for getting different colors through ArrayList interface and search whether
+// the color "Red" is available or not
+
 package ArrayList;
 import java.util.ArrayList;
 public class ColorManager {
