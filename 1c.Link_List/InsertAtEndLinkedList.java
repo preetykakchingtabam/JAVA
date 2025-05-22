ff73e4ea2ca057afa37b6b006b510a2839cc6153
preetykakchingtabam <pitykakchingtabam@gmail.com>
2025-05-22 23:21:44 +0530

    Update InsertAtEndLinkedList.java

--- 1c.Link_List/InsertAtEndLinkedList.java
+++ 1c.Link_List/InsertAtEndLinkedList.java
@@ -1,3 +1,6 @@
+// 3. Write a Java program to insert the specified element at the end of a linked list.( using
+// l_listobj.offerLast("Pink"))
+
 package LinkedList;
 import java.util.LinkedList;
 public class InsertAtEndLinkedList {
