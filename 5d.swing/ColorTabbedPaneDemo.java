65fc6446f16e6ab62fb33cf19493231348c8f4dd
preetykakchingtabam <pitykakchingtabam@gmail.com>
2025-05-22 23:35:32 +0530

    Update ColorTabbedPaneDemo.java

--- 5d.swing/ColorTabbedPaneDemo.java
+++ 5d.swing/ColorTabbedPaneDemo.java
@@ -1,3 +1,6 @@
+// 5d. Develop a Swing program in Java to create a Tabbed Pan of Cyan, Magenta and Yellow and
+// display the concerned color whenever the specific tab is selected in the Pan
+
 package swing5d;
 import javax.swing.*;
 import java.awt.*;
