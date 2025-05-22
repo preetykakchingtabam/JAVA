ea4ad6dc549a76afea8fc088b45a3ee65109be82
preetykakchingtabam <pitykakchingtabam@gmail.com>
2025-05-22 23:25:19 +0530

    Update NullOrWhitespaceCheck.java

--- 3b.String_Exercise/NullOrWhitespaceCheck.java
+++ 3b.String_Exercise/NullOrWhitespaceCheck.java
@@ -1,3 +1,6 @@
+// Q1. Write a Java Program for Checking if a given string is null or contains only whitespace using user
+// defined function isNullOrEmpty().
+
 package StringExercise;
 
 public class NullOrWhitespaceCheck {
