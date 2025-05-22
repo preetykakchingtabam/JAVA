f3f4f3aafd16bf461ead885fafa8a3c1942bf08d
preetykakchingtabam <pitykakchingtabam@gmail.com>
2025-05-22 23:33:40 +0530

    Update ImageButtonApp.java

--- 4c.swing/ImageButtonApp.java
+++ 4c.swing/ImageButtonApp.java
@@ -1,3 +1,7 @@
+// 4c. Develop a Swing program in Java to display a message “Digital Clock is pressed” or “Hour
+// Glass is pressed” depending upon the Jbutton with image either Digital Clock or Hour Glass is
+// pressed by implementing the event handling mechanism with addActionListener( ).
+
 package swing4c;
 
 import javax.swing.*;
